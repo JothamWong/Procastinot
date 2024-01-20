40386457f29376097df948e5fc7b38c60d8fbd80
arnav-ag <arnav.ctf@gmail.com>
2024-01-20 18:38:19 +0800

    manifest no conflicts

--- app/src/main/java/com/example/myapplication/MyAccessibilityService.kt
+++ app/src/main/java/com/example/myapplication/MyAccessibilityService.kt
@@ -1,16 +1,23 @@
 package com.example.myapplication
 
+import android.accessibilityservice.AccessibilityGestureEvent
 import android.accessibilityservice.AccessibilityService
+import android.accessibilityservice.AccessibilityServiceInfo
 import android.os.Build
+import android.os.Bundle
 import android.view.accessibility.AccessibilityEvent
+import android.view.accessibility.AccessibilityNodeInfo
 import androidx.annotation.RequiresApi
 
 class MyAccessibilityService : AccessibilityService() {
+    private var scrollCoefficient = 1.0
+
 
     override fun onServiceConnected() {
         super.onServiceConnected()
         println("Service connected")
     }
+
 
     @RequiresApi(Build.VERSION_CODES.P)
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
@@ -18,6 +25,12 @@ class MyAccessibilityService : AccessibilityService() {
         if (event?.eventType == AccessibilityEvent.TYPE_VIEW_SCROLLED) {
             val scrollX = event.scrollDeltaX
             val scrollY = event.scrollDeltaY
+            println(scrollY)
+            if (scrollY >= 0){
+                println("scrolling backwards")
+                event.source?.performAction(AccessibilityNodeInfo.ACTION_SCROLL_BACKWARD)
+            }
+
             // For now, we'll just log the scroll values
             println("Scroll Event: X=$scrollX, Y=$scrollY")
         }
