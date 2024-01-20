75a00b27870f6b2d47dc7ee6c003d5ccb031600b
amitrahman1026 <amit.amit.rahman@gmail.com>
2024-01-20 20:40:09 +0800

    window change dont work shits broke

--- app/src/main/java/com/example/myapplication/MyAccessibilityService.kt
+++ app/src/main/java/com/example/myapplication/MyAccessibilityService.kt
@@ -1,13 +1,12 @@
 package com.example.myapplication
 
-import android.accessibilityservice.AccessibilityGestureEvent
 import android.accessibilityservice.AccessibilityService
-import android.accessibilityservice.AccessibilityServiceInfo
+import android.app.usage.UsageStatsManager
+import android.content.Context
 import android.os.Build
-import android.os.Bundle
 import android.view.accessibility.AccessibilityEvent
-import android.view.accessibility.AccessibilityNodeInfo
 import androidx.annotation.RequiresApi
+import java.util.Calendar
 
 class MyAccessibilityService : AccessibilityService() {
     private var scrollCoefficient = 1.0
@@ -22,18 +21,38 @@ class MyAccessibilityService : AccessibilityService() {
     @RequiresApi(Build.VERSION_CODES.P)
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
         println(event)
-        if (event?.eventType == AccessibilityEvent.TYPE_VIEW_SCROLLED) {
-            val scrollX = event.scrollDeltaX
-            val scrollY = event.scrollDeltaY
-            println(scrollY)
-            if (scrollY >= 0){
-                println("scrolling backwards")
-                event.source?.performAction(AccessibilityNodeInfo.ACTION_SCROLL_BACKWARD)
-            }
 
-            // For now, we'll just log the scroll values
-            println("Scroll Event: X=$scrollX, Y=$scrollY")
+        if (event?.eventType == AccessibilityEvent.TYPE_WINDOWS_CHANGED) {
+            println("Shits in focus")
+            val packageName = event.packageName.toString()
+            if (trackedPackages.contains(packageName)) {
+                val currentTime = System.currentTimeMillis()
+
+                // Init package
+                if (!appForegroundTime.containsKey(packageName)) {
+                    appForegroundTime[packageName] = currentTime
+                } else {
+                    // Increment the foreground time
+                    val startTime = appForegroundTime[packageName] ?: currentTime
+                    val timeSpent = currentTime - startTime
+                    appForegroundTime[packageName] = timeSpent
+                }
+            }
         }
+        if (event?.eventType == AccessibilityEvent.TYPE_VIEW_SCROLLED) {
+            val scrollY = event.scrollDeltaY
+
+            val timeSpent = getTotalTimeInForeground(event.packageName.toString())
+            // For now, we'll just log the scroll values
+            println("Scroll Event: Y=$scrollY, Time Spent=$timeSpent")
+        }
+    }
+
+    private val trackedPackages = setOf("com.reddit.frontpage")
+    private val appForegroundTime = hashMapOf<String, Long>()
+
+    private fun getTotalTimeInForeground(packageName: String): Long {
+        return appForegroundTime[packageName] ?: 0L
     }
 
     override fun onInterrupt() {
